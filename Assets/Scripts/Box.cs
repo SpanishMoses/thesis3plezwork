ad50c773d8f690f1c56899dcc41e5ac2a8e50e84
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-10-30 21:41:08 -0400

    block pushing is a go

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public bool canPush;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canPush = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPush == false){
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
+        if (canPush == true){
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player"){
+            canPush = true;
+        }
+
+        if (collision.gameObject.tag == "Player" && collision.gameObject.tag == "Dog"){
+            canPush = false;
+        } 
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canPush = false;
+        }
+
+        if (collision.gameObject.tag == "Player" && collision.gameObject.tag == "Dog")
+        {
+            canPush = false;
+        }
+    }
+}
