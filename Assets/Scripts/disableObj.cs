58bcb6171ebf6cff5a7593d1c88d8f2a028823b0
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2021-04-18 20:36:42 -0400

    cutscene 2 mostly done

---
+++ Assets/Scripts/disableObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableObj : MonoBehaviour
+{
+
+    public GameObject disable;
+    public GameObject enable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void doIt(){
+        disable.SetActive(false);
+        enable.SetActive(true);
+    }
+}
